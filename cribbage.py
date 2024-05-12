0992eabd8deb697917626917750d260ec87dadf1
Aiden Favish <aidenfavish@gmail.com>
2024-05-12 02:24:52 -0700

    pegging works! just no bonuses yet

--- cribbage.py
+++ cribbage.py
@@ -27,13 +27,18 @@ class Card:
         else:
             self.sprite = helper.TimeBasedSprite(f"{self.value}_{self.suit}.png", scale=0.5, start_center_x=self.sprite.center_x, start_center_y=self.sprite.center_y, duration=self.sprite.duration - self.sprite.elapsed_time, end_center_x=self.sprite.end_center_x, end_center_y=self.sprite.end_center_y)
 
+    def clipped_value(self):
+        if self.value > 10:
+            return 10
+        return self.value
+
 class Player:
     def __init__(self, x, y, buttonList):
         self.state = "Locked"
         self.hand: List[Card] = []
         self.hand_pos = (x, y)
         self.increment = 30
-        self.button_list = buttonList
+        self.button_list: List[helper.Button] = buttonList
         self.reveal_button = helper.ClassicButton(buttonList, x, y, 225, 50, "Click to Reveal", self.reveal, hidden=True)
 
         # Crib discard
@@ -43,7 +48,10 @@ class Player:
         self.task = "Discard"
 
         # Pegging
+        self.going = False
         self.select_button = helper.ClassicButton(buttonList, x, y + 150, 250, 50, "Select", self.select, hidden=True)
+        self.go_button = helper.ClassicButton(buttonList, x, y + 150, 250, 50, "Go", self.go, hidden=True)
+        
 
     def update(self, dt):
         for card in self.hand:
@@ -51,13 +59,13 @@ class Player:
 
             if self.state == "Hidden" and card.sprite.elapsed_time > card.sprite.duration:  # If cards aren't animating then display button
                 self.state = "To be revealed"
-                self.reveal_button.hidden = False
 
     def draw(self):
         for card in self.hand:
             card.sprite.draw()
 
         if self.state == "To be revealed":
+            self.reveal_button.hidden = False
             self.reveal_button.draw()
 
         if self.state == "Revealed" and len(self.crib_discard) == 2 and self.task == "Discard":
@@ -66,6 +74,9 @@ class Player:
         elif self.state == "Revealed" and len(self.crib_discard) == 1 and self.task == "Pegging":
             self.select_button.hidden = False
             self.select_button.draw()
+        elif self.state == "Revealed" and self.task == "Going" and not self.going:
+            self.go_button.hidden = False
+            self.go_button.draw()
 
     def new_hand(self, hand: List[Card]):
         self.hand = hand
@@ -82,7 +93,6 @@ class Player:
             self.fresh_buttons()
 
     def clicked(self, index):
-        print(f"Clicked: {index}")
         if self.state == "Revealed":
             if index in self.crib_discard:
                 self.crib_discard.remove(index)
@@ -101,7 +111,7 @@ class Player:
             self.crib_discard = [self.hand[self.crib_discard[0]], self.hand[self.crib_discard[1]]]
             self.hand.remove(self.crib_discard[0])
             self.hand.remove(self.crib_discard[1])
-            self.button_list = self.button_list[6:]
+            self.clip_buttons(6)
             self.organize()
             self.state = "Done"
             for card in self.hand:
@@ -120,7 +130,7 @@ class Player:
 
     def confirm(self):
         self.hand.remove(self.crib_discard[0])
-        self.button_list = self.button_list[len(self.hand) + 1:]
+        self.clip_buttons(len(self.hand) + 1)
         self.organize()
 
     def deny(self):
@@ -137,6 +147,16 @@ class Player:
         for i in range(len(self.hand)):
             button = helper.ClassicButton(temp, start_x + self.increment * i, self.hand_pos[1], self.hand[i].sprite.width, self.hand[i].sprite.height, "", lambda x=i: self.clicked(x))
             self.button_list.insert(0,button)
+
+    def go(self):
+        self.going = True
+        self.go_button.hidden = True
+
+    def clip_buttons(self, x):
+        temp = self.button_list[x:]
+        self.button_list.clear()
+        for button in temp:
+            self.button_list.append(button)
 
 class CribbageGame():
     def __init__(self, app):
@@ -158,14 +178,19 @@ class CribbageGame():
         self.player2_round = 0
         self.peg_pot = 0
         self.peg_pot_history = []
+        self.player1_hand_history = []
+        self.player2_hand_history = []
+        self.p1_peg = []
+        self.p2_peg = []
+        self.increment = 30
         self.p1_progress = helper.ProgressBar(175, 575, 300, 45, arcade.color.BLUE, arcade.color.LIGHT_BLUE)
         self.p2_progress = helper.ProgressBar(175, 475, 300, 45, arcade.color.RED, arcade.color.LIGHT_APRICOT)
 
         # Sets up the initial scene
         self.shuffled_cards[-1].flip()
         self.shuffled_cards[-2].flip()
-        self.shuffled_cards[-1].sprite.move_to(100, 100, 2)
-        self.shuffled_cards[-2].sprite.move_to(900, 100, 2)
+        self.shuffled_cards[-1].sprite.move_to(250, 100, 2)
+        self.shuffled_cards[-2].sprite.move_to(750, 100, 2)
 
     def update(self, dt):
         # Update all the cards
@@ -179,26 +204,41 @@ class CribbageGame():
         # Update crib
         for card in self.crib:
             card.sprite.move(dt)
-        
+
+        # Debug
+        print(f"current task: {self.currrent_task}")
+        if self.currrent_task == "Placing" or self.currrent_task == "Going" or self.currrent_task == "Pegging" or self.currrent_task == "Clear run":
+            print(f"turn state: {self.turn.state}")
+            print(f"non-turn state: {self.non_turn.state}")
+            print(f"turn task: {self.turn.task}")
+            print(f"non-turn task: {self.non_turn.task}")
+            print(f"peg pot: {self.peg_pot}")
+            print(f"peg pot history: {len(self.peg_pot_history)}")
+            print(f"player 1 score: {self.player1_round}")
+            print(f"player 2 score: {self.player2_round}")
+            print(f"buttons: {len(self.player1.button_list)}")
+
         # Update the current task
         if self.currrent_task == "Pick Dealer":
             temp = self.freeze_scene - dt
             if temp < 0:
                 if self.shuffled_cards[-1].value == self.shuffled_cards[-2].value:  # Redraw
+                    self.shuffled_cards = self.get_shuffled_deck()  # Reshuffle deck
                     self.freeze_scene = 5.0
                     self.shuffled_cards[-1].flip()
                     self.shuffled_cards[-2].flip()
-                    self.shuffled_cards[-1].sprite.move_to(100, 100, 2)
-                    self.shuffled_cards[-2].sprite.move_to(900, 100, 2)
+                    self.shuffled_cards[-1].sprite.move_to(250, 100, 2)
+                    self.shuffled_cards[-2].sprite.move_to(750, 100, 2)
                 elif self.shuffled_cards[-1].value > self.shuffled_cards[-2].value:  # Lowest value deals
                     self.dealer = self.player2  # player 2 is the dealer
                     self.non_dealer = self.player1  # player 1 is the non-dealer
                     self.currrent_task = "Draw Hand"
+                    self.shuffled_cards = self.get_shuffled_deck()  # Reshuffle deck
                 else:
                     self.dealer = self.player1  # player 1 is the dealer
                     self.non_dealer = self.player2  # player 2 is the non-dealer
                     self.currrent_task = "Draw Hand"
-                self.shuffled_cards = self.get_shuffled_deck()  # Reshuffle deck
+                    self.shuffled_cards = self.get_shuffled_deck()  # Reshuffle deck
 
             elif temp < 2 and not self.shuffled_cards[-1].on_back:
                 # Bring cards back to deck
@@ -256,32 +296,45 @@ class CribbageGame():
             self.currrent_task = "Pegging"
             self.turn = self.non_dealer
             self.non_turn = self.dealer
+            self.player1_hand_history = self.player1.hand.copy()
+            self.player2_hand_history = self.player2.hand.copy()
 
         elif self.currrent_task == "Pegging":  # Start the pegging
-            self.turn.task = "Pegging"
             temp = False
             for card in self.turn.hand:
                 if card.value + self.peg_pot <= 31:
                     temp = True
                     break
             if temp:  # Place allowed card
+                self.turn.task = "Pegging"
                 self.currrent_task = "Placing"
                 self.turn.state = "To be revealed"
-            else:  # "Go" to next player
+            elif self.turn.going or self.turn.hand == []:  # "Go" to next player
+                self.turn.going = False
+                self.turn.flip_hand()
+                self.turn.clip_buttons(len(self.turn.hand))
+                self.turn.state = "Locked"
                 self.currrent_task = "Going"
+                self.non_turn.state = "To be revealed"
+            elif self.turn.task != "Going": 
+                self.turn.state = "To be revealed"
+                self.turn.task = "Going"
 
         elif self.currrent_task == "Placing":  # Place a card
             if self.turn.state == "Selected" and self.turn.crib_discard[0].value + self.peg_pot <= 31:
-                self.peg_pot += self.turn.crib_discard[0].value
+                self.peg_pot += self.turn.crib_discard[0].clipped_value()
                 self.turn.confirm()
                 self.turn.flip_hand()
-                self.turn.crib_discard[0].sprite.move_to(250 if self.turn == self.player1 else 750, 325, 1)
+                self.add_p_peg(self.turn, self.turn.crib_discard[0])
+                #self.turn.crib_discard[0].sprite.move_to(250 if self.turn == self.player1 else 750, 365, 1)
                 self.peg_pot_history.append((self.turn, self.turn.crib_discard[0].value))
                 
                 # Check for points save in history
                 if self.peg_pot == 31:
                     self.add_points(self.turn, 2)
-                    self.peg_pot_history = []
+                    self.currrent_task = "Clear run"
+                else:
+                    self.currrent_task = "Next turn"
 
                 self.turn.crib_discard = []
                 self.turn.state = "Locked"
@@ -301,22 +354,60 @@ class CribbageGame():
                 if self.peg_pot == 31:
                     self.add_points(self.non_turn, 2)
                 else:
-                    self.add_points(self.non_turn, 1)
-                self.peg_pot = 0
+                    self.add_points(self.non_turn, 1)  # Gives go points
+
+                self.non_turn.clip_buttons(len(self.non_turn.hand))
+                self.non_turn.state = "Locked"
+                if self.non_turn.hand != [] and not self.non_turn.hand[0].on_back:
+                    self.non_turn.flip_hand()
+                self.currrent_task = "Clear run"
+
             elif self.non_turn.state == "Selected" and self.non_turn.crib_discard[0].value + self.peg_pot <= 31:
-                self.peg_pot += self.non_turn.crib_discard[0].value
+                self.peg_pot += self.non_turn.crib_discard[0].clipped_value()
                 self.non_turn.confirm()
-                self.non_turn.crib_discard[0].sprite.move_to(250 if self.non_turn == self.player1 else 750, 325, 1)
+                self.add_p_peg(self.non_turn, self.non_turn.crib_discard[0])
+                #self.non_turn.crib_discard[0].sprite.move_to(250 if self.non_turn == self.player1 else 750, 365, 1)
                 self.peg_pot_history.append((self.non_turn, self.non_turn.crib_discard[0].value))
 
                 # Check for points save in history
-                
+
                 self.non_turn.crib_discard = []
                 self.non_turn.state = "Revealed"
                 self.non_turn.fresh_buttons()
             elif self.non_turn.state == "Selected":
                 self.non_turn.state = "Revealed"
                 self.non_turn.deny()
+        elif self.currrent_task == "Next turn":
+            if self.player1.hand == [] and self.player2.hand == []:
+                self.currrent_task = "Clear run"
+                return
+
+            if self.turn == self.player1:
+                self.turn = self.player2
+                self.non_turn = self.player1
+            else:
+                self.turn = self.player1
+                self.non_turn = self.player2
+            self.currrent_task = "Pegging"
+        elif self.currrent_task == "Clear run":
+            if self.player1.hand == [] and self.player2.hand == []:
+                self.add_points(self.peg_pot_history[-1][0], 1)  # Gives last card points
+                self.currrent_task = "Counting"
+                self.player1.state = "Locked"
+                self.player2.state = "Locked"
+            else:
+                # Switch turns
+                if self.player1 == self.peg_pot_history[-1][0]:  # Whoever put the last card down is non-turn
+                    self.turn = self.player2
+                    self.non_turn = self.player1
+                else:
+                    self.turn = self.player1
+                    self.non_turn = self.player2
+                self.currrent_task = "Pegging"
+
+            self.peg_pot = 0
+            self.peg_pot_history = []
+            self.clear_p_peg()
 
     def draw(self):
         if self.currrent_task == "Pick Dealer" or self.currrent_task == "Draw Hand":
@@ -336,6 +427,13 @@ class CribbageGame():
         # Draw the progress bars
         self.p1_progress.on_draw()
         self.p2_progress.on_draw()
+
+        # Draw the pegged cards
+        for card in self.p1_peg:
+            card.sprite.draw()
+
+        for card in self.p2_peg:
+            card.sprite.draw()
 
     def get_shuffled_deck(self):
         deck = []
@@ -359,6 +457,30 @@ class CribbageGame():
             self.p2_progress.set_progress2((self.player2_round + self.player2_total) / 121)
             if self.player2_round + self.player2_total >= 121:
                 pass # Player 2 wins
+
+    def confirm_points(self):
+        self.player1_total += self.player1_round
+        self.player2_total += self.player2_round
+        self.player1_round = 0
+        self.player2_round = 0
+        self.p1_progress.set_progress((self.player1_total) / 121)
+        self.p2_progress.set_progress((self.player2_total) / 121)
+
+    def add_p_peg(self, player, card):
+        if player == self.player1:
+            self.p1_peg.append(card)
+            start_x = 250 - self.increment * (len(self.p1_peg) / 2 - 0.5)
+            for i in range(len(self.p1_peg)):
+                self.p1_peg[i].sprite.move_to(start_x + self.increment * i, 365, 1.0)
+        else:
+            self.p2_peg.append(card)
+            start_x = 750 - self.increment * (len(self.p2_peg) / 2 - 0.5)
+            for i in range(len(self.p2_peg)):
+                self.p2_peg[i].sprite.move_to(start_x + self.increment * i, 365, 1.0)
+
+    def clear_p_peg(self):
+        self.p1_peg = []
+        self.p2_peg = []
 
 class Cribbage(helper.Page):
     def __init__(self, app):
